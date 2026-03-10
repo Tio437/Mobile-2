e6720bc1fcbe19339f524ea075edb0642e172735
Cipunk <pambuditio437@gmail.com>
2026-03-10 22:55:30 +0700

    Menyelesaikan project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,10 +15,81 @@ class MyApp extends StatelessWidget {
       home : Scaffold(
         backgroundColor: Colors.teal,
         body : SafeArea(
-          child: Column(children: [
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
             CircleAvatar(
               radius: 50.0,
-              backgroundColor: Colors.red,
+              backgroundImage: AssetImage('images/images.png'),
+            ),
+            Text(
+              'The Dog',
+              style: GoogleFonts.pacifico(
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: GoogleFonts.sourceSans3(
+                fontSize: 20.0,
+                color: Colors.teal.shade100,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child:Row(
+                children: [
+                  Icon( Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    '+62 812-3456-7890',
+                    style: GoogleFonts.sourceSans3(
+                      fontSize: 20.0, color: Colors.teal.shade900,
+                    ),
+                  ),
+                ],
+                
+              ),
+            ),
+
+            Container(
+              padding: EdgeInsets.all(10.0),
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child:Row(
+                children: [
+                  Icon( Icons.email,
+                    color: Colors.teal,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'example@example.com',
+                    style: GoogleFonts.sourceSans3(
+                      fontSize: 20.0, color: Colors.teal.shade900,
+                    ),
+                  ),
+                ],
+                
+              ),
             ),
           ],
           ),
